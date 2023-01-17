df7d1fd75ca58fc2eded3eb4e1cb912a06da52ab
Takuma Takezawa <t.t.yeah.b5@gmail.com>
2023-01-17 14:26:43 -0800

    functions to establish/close the connection between databases

---
+++ Flask/flaskr/db.py
@@ -0,0 +1,20 @@
+import sqlite3
+import click
+from flask import current_app, g
+# g is a special object that is unique for each request and used to store data for the request
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
